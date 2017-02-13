5ac32c7fb116af9082e05bf43a87e3d348bc4d65
Anish Thite <anishthite@users.noreply.github.com>
2017-02-13 14:29:53 -0500

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,35 +1,10 @@
-// Copyright (c) 2014 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+// Copyright (c) 2017 Anish Thite
 
-/**
- * Get the current URL.
- *
- * @param {function(string)} callback - called when the URL of the current tab
- *   is found.
- */
-  // Most methods of the Chrome extension APIs are asynchronous. This means that
-  // you CANNOT do something like this:
-  //
-  // var url;
-  // chrome.tabs.query(queryInfo, function(tabs) {
-  //   url = tabs[0].url;
-  // });
-  // alert(url); // Shows "undefined", because chrome.tabs.query is async.
-}
+var endTime;
+var currentTime;
 
-/**
- * @param {string} searchTerm - Search term for Google Image search.
- * @param {function(string,number,number)} callback - Called when an image has
- *   been found. The callback gets the URL, width and height of the image.
- * @param {function(string)} errorCallback - Called when the image is not found.
- *   The callback gets a string that describes the failure reason.
- */
+function get_currentTime(){}
 
-function renderStatus(statusText) {
-  document.getElementById('status').textContent = statusText;
-}
-function(errorMessage) {
-      renderStatus('Cannot display image. ' + errorMessage);
-    }
 
+document.write("Period ends at" + endTime);
+document.write("The current time is: " + currentTime);
