d6e3287312fd445e4cd459d8e2f040e7ef466053
Anish Thite <anishthite@live.com>
2017-02-14 00:09:46 -0500

    added buttons

--- popup.js
+++ popup.js
@@ -1,10 +1,22 @@
 // Copyright (c) 2017 Anish Thite
 
-var endTime;
-var currentTime;
+var linebreak = "<br />";
+var today = new Date();
+function get_currentDay(){
+    var currentDayNumber = today.getDay();
+    return currentDay;
+}
+function get_currentTime(){
 
-function get_currentTime(){}
+    var currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+
+    return currentTime;
+}
+function normal(){
+    if (getcurrentDay() == 1)
 
 
-document.write("Period ends at" + endTime);
-document.write("The current time is: " + currentTime);
+}
+document.write (get_currentDay());
+document.write (linebreak);
+document.write("The current time is: " + get_currentTime());
