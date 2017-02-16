e9937f6ac07e647bac21952b49a3f938780c677b
Anish Thite <anishthite@live.com>
2017-02-16 00:06:57 -0500

    added schedule

--- popup.js
+++ popup.js
@@ -2,28 +2,34 @@
 
 var linebreak = "<br />";
 var today = new Date();
+//get current day ( represented by a number)
 function get_currentDay(){
     var currentDayNumber = today.getDay();
     return currentDayNumber;
 }
+//get current time
 function get_currentTime(){
 
-    var currentTime = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
-
+    var currentTimeFull = today.getHours() + ":" + today.getMinutes() + ":" + today.getSeconds();
+    var currentTime = currentTimeFull.substring(0,5);
     return currentTime;
 }
-
+function compute_NormalDay(){
+    // var currentTime = get_currentTime();
+    // var period;
+    // if currentTime
+    var schedule = "Period 1- 9:05, Period 2 - 10:08, Period 3 - 11:09, Period 4 - 12:25, Period 5 - 1:26, Period 6 - 2:30";
+    document.getElementById("demo").innerHTML = schedule;
+}
 function test(){
 
 document.getElementById("demo").innerHTML = "hello";
 
 }   
-document.write (get_currentDay());
-document.write (linebreak);
-document.write("The current time is: " + get_currentTime());
 
+//display
  window.onload = function() {
 
-     document.getElementById('thing').onclick = test;
+     document.getElementById('thing').onclick = compute_NormalDay;
 }
  
