94e3991f545ff811db905171f3d88a642964edab
Anish Thite <anishthite@live.com>
2017-02-16 00:18:23 -0500

    added 200 schedule

--- popup.js
+++ popup.js
@@ -18,7 +18,14 @@ function compute_NormalDay(){
     // var currentTime = get_currentTime();
     // var period;
     // if currentTime
-    var schedule = "Period 1- 9:05, Period 2 - 10:08, Period 3 - 11:09, Period 4 - 12:25, Period 5 - 1:26, Period 6 - 2:30";
+    var schedule = "Period 1- 9:07" + linebreak + "Period 2 - 10:08" + linebreak +  "Period 3 - 11:09" + linebreak + "Period 4 - 11:30, 11:49, 12:08, 12:27" + linebreak + "Period 5 - 1:28" + linebreak + "Period 6 - 2:30";
+    document.getElementById("demo").innerHTML = schedule;
+}
+function compute_200Day(){
+    // var currentTime = get_currentTime();
+    // var period;
+    // if currentTime
+    var schedule = "Period 1- 9:01" + linebreak + "Period 2 - 9:56" + linebreak +  "Period 3 - 10:51" + linebreak + "Period 4 - 11:12, 11:31, 11:50, 12:09" + linebreak + "Period 5 - 1:26" + linebreak + "Period 6 - 2:30";
     document.getElementById("demo").innerHTML = schedule;
 }
 function test(){
@@ -30,6 +37,7 @@ document.getElementById("demo").innerHTML = "hello";
 //display
  window.onload = function() {
 
-     document.getElementById('thing').onclick = compute_NormalDay;
+     document.getElementById('normalday').onclick = compute_NormalDay;
+     document.getElementById('200').onclick = compute_200Day;
 }
  
